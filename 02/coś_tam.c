7c860f09a7defcf8f4892c31ff43ad30722c998f
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-03-09 11:06:08 +0100

    Update coś_tam.c

--- 02/coś_tam.c
+++ 02/coś_tam.c
@@ -3,16 +3,13 @@
 #define SIZE 4
 
 void swap(int* a, int* b);
-int* sortowanie(int* a, int* b, int* c, int* d);
+int* bubble_sort(int* a, int* b, int* c, int* d);
 
 int main(int argc, char const *argv[])
 {
-    /*
-    a = 10,
-    b = 2, 
-    c = 5,
-    d = 1;
-    */
+    //  a = 10; b = 2; c = 5; d = 1;
+    //  10, 2, 5, 1;
+    //  1, 2, 5, 10;
 
     int a, b, c, d;
     scanf("%d", &a);
@@ -20,20 +17,28 @@ int main(int argc, char const *argv[])
     scanf("%d", &c);
     scanf("%d", &d);
 
-    int tab[] = {a, b, c, d};
-    printf("Liczny przed sortoeaniem");
+    //  1 step
+    int tab[] = {a, b, c, d}; // 10, 2, 5, 1 
+    printf("Liczny przed sortowaniem");
     for(int i=0; i<(SIZE+1); i++){
         printf("%d", tab[i]);
     }
 
+    swap(a,b);
+    printf("a = %d, b = %d\n", a,b);
+
+    //  2 step
     printf("Liczby po sortowaniu");
     int nowa_tab[SIZE];
-    nowa_tab[SIZE] = sortowanie(&a, &b, &c, &d);
+    nowa_tab[0] = sortowanie(&a, &b, &c, &d);
+    for(int i=0; i<(SIZE+1); i++){
+        printf("%d", nowa_tab[i]);
+    }
 
     return 0;
 }
 
-int* sortowanie(int* a, int* b, int* c, int* d){
+int* bubble_sort(int* a, int* b, int* c, int* d){
     int tablica[SIZE] = {*a, *b, *c, *d};
 
     for(int i=0; i<SIZE-1; i++){
