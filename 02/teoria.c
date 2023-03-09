047abd2bf491e7812beba1807714684720d34240
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-03-09 16:45:21 +0100

    Update teoria.c

--- 02/teoria.c
+++ 02/teoria.c
@@ -1,36 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//Многомерные массивы
 int main(int argc, char const *argv[])
 {
-    
-
-    int numbers[3][2] = { {1, 2}, {4, 5}, {7, 8} };
-    /*
-    Macierz:
-        (0)(1) 
-    (0)  1	2
-    (1)  4	5
-    (2)  7	8
-    */
-    printf("numbers[1][0] = %d \n", numbers[1][0]); // 4
-    for(int i =0; i < 3; i++)
-    {
-        // проходим по 2 столбцам каждой строки
-        for(int j =0; j<2; j++)
-        {
-            printf("numbers[%d][%d] = %d \n", i, j, numbers[i][j]);
+    int tab[9][9];
+    //  Wypisuwanie tablicy typu tab[][]
+    int k,j; // k-wierszy, j-kolumny;
+    for(k=0; k<9; k++){
+        for(j=0; j<9; j++){
+            tab[k][j] = 7;
+            printf("%d", tab[k][j]);
         }
+        printf("\n");
     }
-
-    int nunmers_2[2][3] = { {1, 2, 4}, {5, 7, 8} };
-    /*
-    Macierz:
-        (0)(1)(2) 
-    (0)  1	2  4 
-    (1)  5	7  8
-    */
-
     return 0;
 }
+
+/*
+    Wynik: Macierz 9x9
+    777777777
+    777777777
+    777777777
+    777777777
+    777777777
+    777777777
+    777777777
+    777777777
+    777777777
+*/
