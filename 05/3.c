c039b16353e3827620e5128d9b9fd5c93fce6316
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-03-31 22:23:23 +0200

    Create 3.c

---
+++ 05/3.c
@@ -0,0 +1,90 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char *read_line();
+char *find_first_char_from_set(const char *input_string, const char *set);
+void safefree(void **pp);
+
+int main(){
+    printf("Input string:\n");
+    char *input_string=read_line();
+    //puts(input_string);
+    
+    printf("Set:\n");
+    char *set=read_line();
+    //puts(set);
+
+    char *literka = find_first_char_from_set(input_string, set);
+    printf("%s\n", literka);
+    printf("%c\n", *literka);
+
+    
+    safefree((void**)&set);
+
+    set = " \t";
+
+    int count = 0;
+    char *licz = find_first_char_from_set(literka, set);
+    count += 1;
+    while(licz != NULL){
+        licz = find_first_char_from_set(licz+1, set);
+        count ++;
+    }
+
+    printf("%d", count);
+    safefree((void**)&input_string);
+    return 0;
+    /*
+    if (isspace(*licz) != 1 )
+    */
+}
+
+char *read_line(){ 
+    char *arr = malloc(1*sizeof(char));
+    int licznik = 0;
+    char c;
+    while(1) { 
+        c=getchar();
+        if(c=='\n')
+        {
+            break;
+        }
+
+        licznik++;
+        arr = realloc(arr, (licznik+1) * sizeof(char));
+        *(arr+licznik-1) = c;
+    }
+    *(arr+licznik)='\0';
+    return arr; 
+}
+
+char *find_first_char_from_set(const char *input_string, const char *set){
+    const char *literka = NULL;
+    
+    const char *ptr = input_string; //szukamy znaki
+    const char *pptr = set; //znaki
+
+    int size_input = strlen(input_string);
+    int size_set = strlen(set);
+
+    for(int i = 0; i < size_input; i++){
+
+        for (int j = 0; j < size_set; j++){
+
+            if(*(pptr+j) == *(ptr+i)){
+                literka = (ptr+i);
+                return (char *)literka;
+            }
+        }
+    }
+    return NULL;
+}
+
+void safefree(void**pp){
+    if(*pp != NULL){
+        free(*pp);
+    }
+    *pp = NULL;
+}
