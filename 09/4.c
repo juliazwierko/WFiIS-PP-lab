453ce3c68886c67a4724ad30dfc6024f0082e47f
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-12 18:31:43 +0200

    Create 4.c

---
+++ 09/4.c
@@ -0,0 +1,11 @@
+// (0 pkt.) Sprawdź i wypisz, ile bajtów zajmuje struktura typu Student. Następnie sprawdź i wypisz,
+// ile wynosi suma pamięci zajętej przez poszczególne pola struktury. Czy wartości te są równe? 
+//   Jeśli tak, to zmień o 1 liczbę znaków w tablicy przechowującej adres.
+// Następnie do programu dołącz deklarację unii:
+// typedef union
+// {
+//   Student student;
+//   int number;
+// } test_union;
+// Sprawdź i wypisz, ile bajtów zajmuje ta unia. Spróbuj ją wykorzystać, przypisując studenta i wypisując jego dane, 
+// a następnie uzupełniając pole number i wypisując je.
