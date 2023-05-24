006f3ec16318615b39b995deeffc4fe31eea1c4d
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-24 17:44:14 +0200

    Create 1.c

---
+++ 10_test_2/test/1.c
@@ -0,0 +1,2 @@
+1) Zadanie gdzie trzeba zadeklarować wskaźnik / funkcję (przyjmującą coś i zwracającą np. strukturę). 
+  Analogicznie do tego, co wysłała na UPELu. Nie pamiętam dokładnie jaka to była, ale mega prosta deklaracja.
