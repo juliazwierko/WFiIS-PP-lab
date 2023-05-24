c4bbd77992fb947ece4ac8111f7d12d1e0c30630
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-24 20:05:10 +0200

    Create 3.c

---
+++ 10_test_2/test/3.c
@@ -0,0 +1,4 @@
+// Dwuwymiarowa tablica char'ów i wskaźniki (2x do wiersza i 1x do chara). 
+// Umieć zadeklarować te wskaźniki na podstawie ich definicji (przypisania),
+// czyli wiedzieć jakiego mają być typu. Trzeba też wiedzieć co się wypisze, 
+// gdy je zdereferencjujemy wewnątrz printf'a.
