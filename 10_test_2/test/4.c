c4db26b49ad4c01d22dd6432352f3c144c898e6e
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-24 20:05:44 +0200

    Create 4.c

---
+++ 10_test_2/test/4.c
@@ -0,0 +1,10 @@
+//  Wybrać prawidłowy opis deklaracji (z listy drop-down). 
+/*
+void (*ddd[8])(struct Student, ...);
+
+float *(*(*ccc)(float))[5];
+
+float *(*bbb(float))[5];
+
+struct Student *(*aaa)(void);
+*/
