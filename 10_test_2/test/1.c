79efae74cf0f5fa10b13c4bd79879e4539578b70
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-25 12:35:30 +0200

    Update 1.c

--- 10_test_2/test/1.c
+++ 10_test_2/test/1.c
@@ -1,2 +1,35 @@
-1) Zadanie gdzie trzeba zadeklarować wskaźnik / funkcję (przyjmującą coś i zwracającą np. strukturę). 
-  Analogicznie do tego, co wysłała na UPELu. Nie pamiętam dokładnie jaka to była, ale mega prosta deklaracja.
+// 1) Zadanie gdzie trzeba zadeklarować wskaźnik / funkcję (przyjmującą coś i zwracającą np. strukturę). 
+//   Analogicznie do tego, co wysłała na UPELu. Nie pamiętam dokładnie jaka to była, ale mega prosta deklaracja.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    double x;
+    double *px;
+    double **ppx;
+    /*
+    double fun(int);
+    double *fun2(int);
+    double **fun3(int);
+    double (*fun_ptr1)(int);
+    double *(*fun_ptr2)(int);
+    double **(*fun_ptr3)(int);
+    double tab[10];
+    double *tab[10];
+    double (*ptr)[10];
+    double *(*ptr)[10];
+    double **tab[10];
+    double (*tab6[10])(int);
+    double *(*tab7[10])(int);
+    double **(*tab8[10](int));
+    double (*fun4(int))[10];
+    double (*(*fun_ptr4)(int))[10];
+    double (*fun5(void))(int);
+    double *(*(*fun_ptr5)(void))(int)
+    double *(*(*(fun6)(int))[10])(char);
+    double *(*(*(*fun_ptr6)(int))[10])(char);
+    double (*(*(*ptr3)(void))(char, float))(int);
+    */
+    return 0;
+}
