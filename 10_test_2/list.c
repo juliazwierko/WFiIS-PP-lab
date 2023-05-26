e1b57ab27f467c6bb4ac636d89c56f0206383c87
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-05-26 12:24:00 +0200

    Create list.c

---
+++ 10_test_2/list.c
@@ -0,0 +1,32 @@
+#include "list.h"
+
+void initialize_list(SortedList *list)
+{
+    list->head = NULL;
+}
+
+void add(SortedList *list, void *data, CMP cmp)
+{
+    /* TODO */
+}
+
+void print_list(const SortedList *list, PRINT print)
+{
+    /* TODO */
+}
+
+void *get_data(const SortedList *list, CMP cmp, const void *data)
+{
+    /* TODO */
+    return NULL;
+}
+
+void remove_data(SortedList *list, CMP cmp, void *data) 
+{
+    /* TODO */
+}
+
+void remove_all(SortedList *list)
+{
+    /* TODO */
+}
