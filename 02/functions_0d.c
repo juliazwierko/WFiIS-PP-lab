72b869b6210112badc4512e8a67048081d7e5519
Julia Zwierko <114949901+juliazwierko@users.noreply.github.com>
2023-08-25 17:50:26 +0200

    Update and rename 1.c to functions_0d.c

---
+++ 02/functions_0d.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+#include "functions_0d.h".
+
+int i_rand(int min, int max){
+    return rand() % (max - min + 1) + min; 
+}
+
+double d_rand(double min, double max){
+    double r = (double)rand() / RAND_MAX;
+    return r * (max - min) + min;
+}
+
+void i_swap(int *restrict first, int *restrict second){
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+void swap(void * first, void * second, size_t size){
+    char temp;
+    char *p_first = first;
+    char *p_second = second;
+    for(size_t i=0; i<size; i++){
+        temp = p_first[i];
+        p_first[i] = p_second[i];
+        p_second[i] = temp;
+    }
+}
